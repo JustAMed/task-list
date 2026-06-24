aef0ec255ff928094fa86c7d89c519157c68d423
Medansh <just.medansh@gmail.com>
2026-06-24 08:04:41 +0530

    added family based tasks for more expansions later

--- parser.py
+++ parser.py
@@ -1,11 +1,24 @@
-string = "add address thingy // address is zero" # for testing only
+string = "task del complete nothig // 2" # for testing only
 
 def parse(string): # prototype
-    parts = string.strip().split(" ", 1) # split query into first word and rest of words
-    action = parts[0]
-    args = parts[1] if len(parts) > 1 else "" # if no arg is provided, "" is given as arg
-    args = args.split(" /// ") # split the args further
-    return (action, args)
+    parts = string.strip().split(" ", 2) # split query into first word, second word and rest of words
+    partsAmt = len(parts)
+
+    family = (parts[0] if partsAmt > 0 else "").lower() # first word
+    action = (parts [1] if partsAmt > 1 else "").lower() # second word
+
+    args = parts[2] if partsAmt > 2 else "" # if no arg is provided, "" is given as arg
+
+    if args:
+        args = [arg.strip() for arg in args.split("//")] # seperates args by "//" and strips them, returning list of args
+    else:
+        args = []
+
+    return {
+        "family": family,
+        "action": action,
+        "args": args,
+    }
 
 if __name__ == "__main__":
     print(parse(string)) # test case
