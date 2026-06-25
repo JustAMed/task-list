301b5c1922c8079ef8906f93e07d32611b480395
Medansh <just.medansh@gmail.com>
2026-06-25 10:22:45 +0530

    Fixed families being unused

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import database
 from parser import parse
 
-actions = { # defines task: associated function
-    "add": database.add_task,
-    "del": database.del_task,
-    "list": lambda args: render_table(database.get_all_tasks()), # ignores passing of args as function does not take arguments
-    "complete": database.complete_task,
-    "toggle": database.toggle_task,
-}
+task_actions = { # defines task: associated function
+        "add": database.add_task,
+        "del": database.del_task,
+        "list": lambda args: render_table(database.get_all_tasks()), # ignores passing of args as function does not take arguments
+        "complete": database.complete_task,
+        "toggle": database.toggle_task,
+    }
 
 families = {
-    "task": actions
-}
+        "task": task_actions
+    }
 
 def main():
     database.init_db() 
@@ -20,7 +20,14 @@ def main():
         parsed = parse(command)
         if command in ["exit", "quit"]:
             return
-        actions.get(parsed["action"], lambda x: print("unknown"))(parsed["args"]) # Run the associated function in actions dict. If no such action exists, print "unknown"
+        
+        try: # get family, if fails then continue
+            family = families[parsed["family"]]
+        except KeyError:
+            print("unknown family")
+            continue
+
+        family.get(parsed["action"], lambda x: print("unknown"))(parsed["args"]) # Run the associated function in actions dict. If no such action exists, print "unknown"
 
 def render_table(tasks): # renders the table for action "list"
     if not tasks: 
@@ -29,6 +36,8 @@ def render_table(tasks): # renders the table for action "list"
     for id, title, completed in tasks: # Prints each task in format: id. title [✓/✗]
         status = "✓" if completed else "✗"
         print(f"{id}. {title} [{status}]")
+
+
 
 if __name__ == "__main__":
     main()
